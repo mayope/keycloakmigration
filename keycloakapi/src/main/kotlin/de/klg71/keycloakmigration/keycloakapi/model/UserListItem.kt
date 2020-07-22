0cc9f2dc3a80c9ba34f6af14057d5789fb02ff27
mthiele <thiele.michael2@googlemail.com>
2020-07-22 10:54:42 +0200

    #21: Keycloak API returns full user information for user lists

--- keycloakapi/src/main/kotlin/de/klg71/keycloakmigration/keycloakapi/model/UserListItem.kt
+++
@@ -1,9 +0,0 @@
-package de.klg71.keycloakmigration.keycloakapi.model
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties
-import java.util.*
-
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-data class UserListItem(val id: UUID, val createdTimestamp: Long, val username: String, val enabled: Boolean,
-                        val emailVerified: Boolean)
