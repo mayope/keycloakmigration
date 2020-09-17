4de3691c4c72524a7be4582aa2a4dea8c1446bb5
Lukas Meisegeier <lukas.meisegeier@ottogroup.com>
2020-09-17 08:46:13 +0200

    removed env from docker file

--- src/docker/entrypoint.sh
+++ src/docker/entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-env
 ARGUMENTS="${KEYCLOAK_CHANGELOG:-/migration/keycloak-changelog.yml} -u ${ADMIN_USER:-admin} -p ${ADMIN_PASSWORD:-admin} -c ${ADMIN_CLIENT:-admin-cli} -r ${LOGIN_REALM:-master} -b ${BASEURL:-http://localhost:8080/auth} --wait-for-keycloak-timeout ${WAIT_FOR_KEYCLOAK_TIMEOUT:-0}"
 
 if [ -z ${WAIT_FOR_KEYCLOAK+x} ]; then echo "dont wait for keycloak"; else ARGUMENTS="$ARGUMENTS --wait-for-keycloak"; fi
@@ -8,5 +7,5 @@ if [ -z ${DISABLE_WARN_ON_UNDEFINED_VARIABLES+x} ]; then echo "warn on errors"; 
 if [ -z ${CORRECT_HASHES+x} ]; then echo "dont correct hashes"; else ARGUMENTS="$ARGUMENTS --correct-hashes"; fi
 if [ -z ${STAY_IDLE+x} ]; then echo "dont stay idle"; else ARGUMENTS="$ARGUMENTS && tail -f /dev/null "; fi
 
-echo $ARGUMENTS
+if [ -z ${PRINT_ARGUMENTS} ]; then echo ""; else echo "$ARGUMENTS"; fi
 /bin/sh -c "java -jar keycloakmigration.jar $ARGUMENTS"
