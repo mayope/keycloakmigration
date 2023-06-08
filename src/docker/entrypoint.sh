019edc3c487072cc7e3b0e68f1eb9f5c20316d3c
sideisra <denny.israel@ottogroup.com>
2023-06-08 13:57:59 +0200

    Fix totp env var handling

--- src/docker/entrypoint.sh
+++ src/docker/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-if [ -z ${ADMIN_TOTP} ]; then TOTP=""; else TOTP="-t ${ADMIN_TOTP}"; fi
-ARGUMENTS="${KEYCLOAK_CHANGELOG:-/migration/keycloak-changelog.yml} -u ${ADMIN_USER:-admin} -p ${ADMIN_PASSWORD:-admin} ${TOTP} -c ${ADMIN_CLIENT:-admin-cli} -r ${LOGIN_REALM:-master} -b ${BASEURL:-http://localhost:8080/auth} --wait-for-keycloak-timeout ${WAIT_FOR_KEYCLOAK_TIMEOUT:-0}"
+if [ -z ${ADMIN_TOTP} ]; then TOTP=""; else TOTP="-t $ADMIN_TOTP"; fi
+ARGUMENTS="${KEYCLOAK_CHANGELOG:-/migration/keycloak-changelog.yml} -u ${ADMIN_USER:-admin} -p ${ADMIN_PASSWORD:-admin} $TOTP -c ${ADMIN_CLIENT:-admin-cli} -r ${LOGIN_REALM:-master} -b ${BASEURL:-http://localhost:8080/auth} --wait-for-keycloak-timeout ${WAIT_FOR_KEYCLOAK_TIMEOUT:-0}"
 
 if [ -z ${WAIT_FOR_KEYCLOAK+x} ]; then echo "dont wait for keycloak"; else ARGUMENTS="$ARGUMENTS --wait-for-keycloak"; fi
 if [ -z ${FAIL_ON_UNDEFINED_VARIABLES+x} ]; then echo "dont fail on errors"; else ARGUMENTS="$ARGUMENTS --fail-on-undefined-variables"; fi
