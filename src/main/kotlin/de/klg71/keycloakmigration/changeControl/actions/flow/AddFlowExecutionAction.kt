09339c90764383df0b319e980511eaaab09fc312
Abigail Cortis <abigail.cortis@rs2.com>
2024-10-08 16:21:46 +0200

    fixed issue related to flow execution config

--- src/main/kotlin/de/klg71/keycloakmigration/changeControl/actions/flow/AddFlowExecutionAction.kt
+++ src/main/kotlin/de/klg71/keycloakmigration/changeControl/actions/flow/AddFlowExecutionAction.kt
@@ -1,22 +1,35 @@
 package de.klg71.keycloakmigration.changeControl.actions.flow
 
 import de.klg71.keycloakmigration.changeControl.actions.Action
+import de.klg71.keycloakmigration.keycloakapi.extractLocationUUID
 import de.klg71.keycloakmigration.keycloakapi.model.AddFlowExecution
+import de.klg71.keycloakmigration.keycloakapi.model.AuthenticatorConfig
+import java.util.*
 
 class AddFlowExecutionAction(
-    realm: String,
-    private val flowAlias: String,
-    private val provider: String
+  realm: String,
+  private val flowAlias: String,
+  private val provider: String,
+  private val executionAlias: String,
+  private val config: Map<String, String> = emptyMap()
 ) : Action(realm) {
 
-    override fun execute() {
-        var response = client.addFlowExecution(realm(), flowAlias, AddFlowExecution(provider))
-        return
-    }
+  private lateinit var executionId: UUID
 
-    override fun undo() {
-        TODO("Not yet implemented")
-    }
+  override fun execute() {
+    executionId = client.addFlowExecution(realm(), flowAlias, AddFlowExecution(provider)).extractLocationUUID()
 
-    override fun name() = "AddFlowExecution [$provider] to $flowAlias"
+    if (config.isNotEmpty())
+      client.updateFlowExecutionWithNewConfiguration(
+        realm(),
+        executionId.toString(),
+        AuthenticatorConfig(executionAlias, config)
+      )
+  }
+
+  override fun undo() {
+    client.deleteFlowExecution(realm(), executionId)
+  }
+
+  override fun name() = "AddFlowExecution [$provider] to $flowAlias"
 }
