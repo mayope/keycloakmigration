02c934a1a1d3763d5bc908c549d8218347820291
Abigail Cortis <abigail.cortis@rs2.com>
2024-10-09 13:57:03 +0200

    refactored CopyFlowAction

--- src/main/kotlin/de/klg71/keycloakmigration/changeControl/actions/flow/CopyFlowAction.kt
+++ src/main/kotlin/de/klg71/keycloakmigration/changeControl/actions/flow/CopyFlowAction.kt
@@ -3,7 +3,6 @@ package de.klg71.keycloakmigration.changeControl.actions.flow
 import de.klg71.keycloakmigration.changeControl.actions.Action
 import de.klg71.keycloakmigration.keycloakapi.copyAuthFlow
 import de.klg71.keycloakmigration.keycloakapi.model.Flow
-import java.util.UUID
 
 class CopyFlowAction(
   realm: String,
