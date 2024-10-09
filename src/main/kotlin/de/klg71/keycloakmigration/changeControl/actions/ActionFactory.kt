b6c2bbb6c318717d4450a235efbf3f949596dcd4
Abigail Cortis <abigail.cortis@rs2.com>
2024-10-09 13:54:52 +0200

    removed import *

--- src/main/kotlin/de/klg71/keycloakmigration/changeControl/actions/ActionFactory.kt
+++ src/main/kotlin/de/klg71/keycloakmigration/changeControl/actions/ActionFactory.kt
@@ -34,7 +34,11 @@ import de.klg71.keycloakmigration.changeControl.actions.clientscope.mapper.AddCl
 import de.klg71.keycloakmigration.changeControl.actions.clientscope.mapper.AddClientScopeUserAttributeMapperAction
 import de.klg71.keycloakmigration.changeControl.actions.clientscope.mapper.AddClientScopeUserRealmRoleMapperAction
 import de.klg71.keycloakmigration.changeControl.actions.clientscope.mapper.DeleteClientScopeMapperAction
-import de.klg71.keycloakmigration.changeControl.actions.flow.*
+import de.klg71.keycloakmigration.changeControl.actions.flow.AddFlowAction
+import de.klg71.keycloakmigration.changeControl.actions.flow.AddFlowExecutionAction
+import de.klg71.keycloakmigration.changeControl.actions.flow.CopyFlowAction
+import de.klg71.keycloakmigration.changeControl.actions.flow.DeleteFlowAction
+import de.klg71.keycloakmigration.changeControl.actions.flow.UpdateFlowAction
 import de.klg71.keycloakmigration.changeControl.actions.group.AddGroupAction
 import de.klg71.keycloakmigration.changeControl.actions.group.AssignRoleToGroupAction
 import de.klg71.keycloakmigration.changeControl.actions.group.DeleteGroupAction
