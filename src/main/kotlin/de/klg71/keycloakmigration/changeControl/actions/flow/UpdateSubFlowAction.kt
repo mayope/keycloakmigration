55656245a4048203c07d54ca542fb6564ff7a90b
Isaac Mercieca <isaac.mercieca@rs2.com>
2025-03-13 12:59:37 +0100

    added tests for `UpdateSubFlowAction`

--- src/main/kotlin/de/klg71/keycloakmigration/changeControl/actions/flow/UpdateSubFlowAction.kt
+++ src/main/kotlin/de/klg71/keycloakmigration/changeControl/actions/flow/UpdateSubFlowAction.kt
@@ -1,50 +1,57 @@
 package de.klg71.keycloakmigration.changeControl.actions.flow
 
 import de.klg71.keycloakmigration.changeControl.actions.Action
+import de.klg71.keycloakmigration.changeControl.actions.MigrationException
 import de.klg71.keycloakmigration.keycloakapi.model.AuthenticationExecution
 import de.klg71.keycloakmigration.keycloakapi.model.Flow
 import de.klg71.keycloakmigration.keycloakapi.model.UpdateFlowExecution
 
 class UpdateSubFlowAction(
-  realm: String?,
-  private val subFlow: String,
-  private val topLevelFlow: String,
-  private val requirement: Flow.Requirement
+    realm: String?,
+    private val subFlow: String,
+    private val topLevelFlow: String,
+    private val requirement: Flow.Requirement
 ) : Action(realm) {
 
-  private lateinit var original: AuthenticationExecution
+    private lateinit var original: AuthenticationExecution
 
-  override fun execute() {
+    override fun execute() {
+        if (client.flows(realm()).none { it.alias == topLevelFlow })
+            throw MigrationException("Flow $topLevelFlow does not exist!")
 
-    original = client.flowExecutions(realm(), topLevelFlow)
-      .first { it.displayName == subFlow && it.authenticationFlow }
+        val flow = client.flowExecutions(realm(), topLevelFlow)
+            .firstOrNull { it.displayName == subFlow && it.authenticationFlow }
 
-    val updateFlowExecution = UpdateFlowExecution(
-      original.id,
-      requirement,
-      original.level,
-      original.index,
-      original.providerId
-    )
+        if (flow == null) throw MigrationException("SubFlow $subFlow doesn't exist for $topLevelFlow!")
 
-    client.updateFlowExecution(
-      realm(),
-      topLevelFlow,
-      updateFlowExecution
-    )
-  }
+        original = flow
 
-  override fun undo() {
-    val updateFlowExecution = UpdateFlowExecution(
-      original.id,
-      original.requirement,
-      original.level,
-      original.index,
-      original.providerId
-    )
+        val updateFlowExecution = UpdateFlowExecution(
+            original.id,
+            requirement,
+            original.level,
+            original.index,
+            original.providerId
+        )
 
-    client.updateFlowExecution(realm(), topLevelFlow, updateFlowExecution)
-  }
+        client.updateFlowExecution(
+            realm(),
+            topLevelFlow,
+            updateFlowExecution
+        )
+    }
 
-  override fun name() = "UpdateSubFlow $subFlow to $requirement"
+    override fun undo() {
+        val updateFlowExecution = UpdateFlowExecution(
+            original.id,
+            original.requirement,
+            original.level,
+            original.index,
+            original.providerId
+        )
+
+        client.updateFlowExecution(realm(), topLevelFlow, updateFlowExecution)
+    }
+
+    override fun name() = "UpdateSubFlow $subFlow to $requirement"
 }
