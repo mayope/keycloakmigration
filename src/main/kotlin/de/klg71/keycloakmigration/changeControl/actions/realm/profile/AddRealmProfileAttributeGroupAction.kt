729cd58e338b3c7e389f37552439bfcbacd17911
Abigail Cortis <abigail.cortis@rs2.com>
2025-05-06 17:37:39 +0200

    initial logic for AddRealmProfileAttributeGroup action

---
+++ src/main/kotlin/de/klg71/keycloakmigration/changeControl/actions/realm/profile/AddRealmProfileAttributeGroupAction.kt
@@ -0,0 +1,37 @@
+package de.klg71.keycloakmigration.changeControl.actions.realm.profile
+
+import de.klg71.keycloakmigration.changeControl.actions.Action
+import de.klg71.keycloakmigration.keycloakapi.model.RealmProfile
+
+class AddRealmProfileAttributeGroupAction(
+    realm: String?,
+    private val name: String,
+    private val displayName: String?,
+    private val displayDescription: String?,
+) : Action(realm) {
+    override fun execute() {
+        val currentProfile = client.realmUserProfile(realm())
+
+//        TODO: add check if group with name already exists
+
+        val newGroup = mapOf(
+            "name" to name,
+            "displayHeader" to (displayName ?: ""),
+            "displayDescription" to (displayDescription ?: "")
+        )
+
+        val newProfile = RealmProfile(
+            currentProfile.attributes,
+            currentProfile.groups + newGroup,
+            currentProfile.unmanagedAttributePolicy
+        )
+
+        client.updateRealmProfile(realm(), newProfile)
+    }
+
+    override fun undo() {
+        TODO("Not yet implemented")
+    }
+
+    override fun name() = "AddRealmProfileAttributeGroup $name"
+}
