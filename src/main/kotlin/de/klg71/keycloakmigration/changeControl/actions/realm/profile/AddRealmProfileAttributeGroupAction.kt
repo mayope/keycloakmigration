d0916f6c7ad4d68b1c44f17d151d6f0e355a3361
Abigail Cortis <abigail.cortis@rs2.com>
2025-05-07 09:34:13 +0200

    added undo logic

--- src/main/kotlin/de/klg71/keycloakmigration/changeControl/actions/realm/profile/AddRealmProfileAttributeGroupAction.kt
+++ src/main/kotlin/de/klg71/keycloakmigration/changeControl/actions/realm/profile/AddRealmProfileAttributeGroupAction.kt
@@ -28,7 +28,21 @@ class AddRealmProfileAttributeGroupAction(
   }
 
   override fun undo() {
-    TODO("Not yet implemented")
+    val currentProfile = client.realmUserProfile(realm())
+
+    val group = mapOf(
+      "name" to name,
+      "displayHeader" to (displayName ?: ""),
+      "displayDescription" to (displayDescription ?: "")
+    )
+
+    val indexToRemove = currentProfile.groups.indexOfLast { it == group }
+
+    val revertedGroups = currentProfile.groups.filterIndexed { index, _ -> index != indexToRemove }
+
+    val newProfile = RealmProfile(currentProfile.attributes, revertedGroups, currentProfile.unmanagedAttributePolicy)
+
+    client.updateRealmProfile(realm(), newProfile)
   }
 
   override fun name() = "AddRealmProfileAttributeGroup $name"
