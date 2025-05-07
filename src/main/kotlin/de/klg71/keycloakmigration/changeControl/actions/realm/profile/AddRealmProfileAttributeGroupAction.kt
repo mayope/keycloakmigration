c05e0cff39263bd6c76da71ebdb21178efa1c096
Abigail Cortis <abigail.cortis@rs2.com>
2025-05-07 09:22:53 +0200

    reformat

--- src/main/kotlin/de/klg71/keycloakmigration/changeControl/actions/realm/profile/AddRealmProfileAttributeGroupAction.kt
+++ src/main/kotlin/de/klg71/keycloakmigration/changeControl/actions/realm/profile/AddRealmProfileAttributeGroupAction.kt
@@ -4,32 +4,32 @@ import de.klg71.keycloakmigration.changeControl.actions.Action
 import de.klg71.keycloakmigration.keycloakapi.model.RealmProfile
 
 class AddRealmProfileAttributeGroupAction(
-    realm: String?,
-    private val name: String,
-    private val displayName: String?,
-    private val displayDescription: String?,
+  realm: String?,
+  private val name: String,
+  private val displayName: String?,
+  private val displayDescription: String?,
 ) : Action(realm) {
-    override fun execute() {
-        val currentProfile = client.realmUserProfile(realm())
+  override fun execute() {
+    val currentProfile = client.realmUserProfile(realm())
 
-        val newGroup = mapOf(
-            "name" to name,
-            "displayHeader" to (displayName ?: ""),
-            "displayDescription" to (displayDescription ?: "")
-        )
+    val newGroup = mapOf(
+      "name" to name,
+      "displayHeader" to (displayName ?: ""),
+      "displayDescription" to (displayDescription ?: "")
+    )
 
-        val newProfile = RealmProfile(
-            currentProfile.attributes,
-            currentProfile.groups + newGroup,
-            currentProfile.unmanagedAttributePolicy
-        )
+    val newProfile = RealmProfile(
+      currentProfile.attributes,
+      currentProfile.groups + newGroup,
+      currentProfile.unmanagedAttributePolicy
+    )
 
-        client.updateRealmProfile(realm(), newProfile)
-    }
+    client.updateRealmProfile(realm(), newProfile)
+  }
 
-    override fun undo() {
-        TODO("Not yet implemented")
-    }
+  override fun undo() {
+    TODO("Not yet implemented")
+  }
 
-    override fun name() = "AddRealmProfileAttributeGroup $name"
+  override fun name() = "AddRealmProfileAttributeGroup $name"
 }
